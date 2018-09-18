706876ae39439a114cb23ee1644836015c137157
SashaJohnson <alexjohnson@yandex.ru>
2018-09-19 01:28:14 +0300

    added block of code which identifies var a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+ 
+// example: a * x^2 - 10 * x + 12
+
+for (var i = 0; i < equation.length; i++) {
+    
+    if (equation[i] == ' ') {
+        
+        var a = parseInt(equation.slice(0, equation.indexOf(equation[i])));
+        
+        console.log(a);
+        
+        break;
+        
+    }
+    
+}
+
+
 }
