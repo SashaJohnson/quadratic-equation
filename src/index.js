b5d9ec7dc0a5cb55837226206cc5363bbc4f2fdf
SashaJohnson <alexjohnson@yandex.ru>
2018-09-19 16:38:22 +0300

    added code to identify all vars, perform calculation and return value

--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
 module.exports = function solveEquation(equation) {
  
-// example: a * x^2 - 10 * x + 12
+// example: 2 * x^2 - 10 * x + 12
+
+var count = 0;
 
 for (var i = 0; i < equation.length; i++) {
     
-    if (equation[i] == ' ') {
+    if (equation[i] == ' ') { count++ }	
+	
+	
+	
+	if (equation[i] == ' ' && count == 1) {
         
         var a = parseInt(equation.slice(0, equation.indexOf(equation[i])));
-        
-        console.log(a);
-        
-        break;
-        
-    }
+	
+	} else if (equation[i] == ' ' && count == 3) {		
+		
+		var bStrStart = i + 1;
+		
+	} else if (equation[i] == ' ' &&  count == 5)  {
+	
+		var bStrEnd = i;
+		
+	} else if (equation[i] == ' ' && count == 7) {
+	
+        var cStrStart = i + 1;
+        var cStrEnd = equation.length;
     
+	} else {continue}
+    
+	
 }
 
+	var bStrFull = equation.slice(bStrStart, bStrEnd);
+	var b = parseInt(bStrFull.replace(' ', ''));
+	
+	var cStrFull = equation.slice(cStrStart, cStrEnd);
+	var c = parseInt(cStrFull.replace(' ', ''));
+	
+	console.log(a,b,c);
+	
+	// finding returns:
+	
+	var d = Math.pow(b, 2) - 4 * a * c;
+	
+	var answer_1 = (-b +Math.sqrt(d)) / (2 * a);
+	
+	var answer_2 = (-b -Math.sqrt(d)) / (2 * a);
+	
+	console.log(answer_1, answer_2);
+	
+	// sorting
+	
+	var response = [answer_1, answer_2].sort();
+	
+	return response;
 
 }
+
